bc0532beefdd45c9c6541341e24508182a637d05
Zoldyg-1 <ikhantkhinin49@gmail.com>
2026-08-15 16:18:31 +0100

    Add precompiled header

---
+++ src/pch.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <RE/Skyrim.h>
+#include <SKSE/SKSE.h>
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cctype>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <format>
+#include <limits>
+#include <mutex>
+#include <optional>
+#include <span>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+#include "CCA/SKEEAPI.h"
